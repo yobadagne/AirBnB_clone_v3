b84448eeb09b35177fe764a45211024780c7d96f
yobadagne <yobadagne@gmail.com>
2023-05-01 18:48:01 +0000

    3

---
+++ api/v1/app.py
@@ -0,0 +1,16 @@
+#!flask/bin/python
+from flask import Flask
+import os
+from models import storage
+from api.v1.views import app_views
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown:
+        """ close storage """
+            storage.close()
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', '5000')
+    app.run(host=host, port=int(port), threaded=True)
