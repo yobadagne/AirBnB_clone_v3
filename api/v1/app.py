d5ccc677e736931dab6dd9cd93c3a1a2ff722704
yobadagne <yobadagne@gmail.com>
2023-05-01 19:07:18 +0000

    3

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown:
-        """ close storage """
-            storage.close()
+def teardown(err):
+    """ close storage """
+    storage.close()
 if __name__ == "__main__":
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = os.environ.get('HBNB_API_PORT', '5000')
